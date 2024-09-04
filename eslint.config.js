5dd04735116bd08e7bef9e32d1732cc61fbd687d
xerxes <odilbekovjavohir4@gmail.com>
2024-09-04 17:40:44 +0500

    fixed eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,vue}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn'
+    }
+  }
+];
